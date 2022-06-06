0a4b5a57cd35f9d65a698818499187196d60382f
ittoufiq <ittoufiq@gmail.com>
2022-06-07 04:12:01 +0600

    How to change variable value and how to use constant

---
+++ variableconst.php
@@ -0,0 +1,28 @@
+<?php
+
+// PHP variable value is changeable. Constant value is not  changeable
+
+// How to check constant variable 
+
+// How to change php variable value 
+
+$task = 'read';
+echo $task;
+
+echo "\n";
+
+$task = 'write';
+echo $task;
+
+echo "\n";
+
+// How to check constant variable 
+
+define('greetings', 'Hello World');
+
+echo constant('greetings');
+// alternative ways 
+echo "\n";
+$const = 'constant';
+
+echo "This value is constant {$const('greetings')}";
