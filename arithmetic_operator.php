ce1f90622326ad2658f701da4adf377785edc3e4
ittoufiq <ittoufiq@gmail.com>
2022-06-09 04:24:39 +0600

    I have completed arithmetic operators prctice

---
+++ arithmetic_operator.php
@@ -0,0 +1,40 @@
+<?php
+// Arithmetic operators & How it works 
+
+// +, -, *, /, % 
+// addition 
+$addition = 12;
+//$number = $number + 13; // = assignment operator 
+$addition += 13; // Alternative process for adition +=
+echo $addition;
+
+echo "\n";
+//subtraction
+$subtraction = 12;
+//$number = $number + 13; // = assignment operator 
+$subtraction -= 13; // Alternative process for subtraction +=
+echo $subtraction;
+
+echo "\n";
+
+// Multiplication 
+$multiplication = 12; // = assignment operator 
+// $multiplication = $multiplication * 12; // Alternative process for Multiplication *=
+$multiplication *= 13;
+echo $multiplication;
+
+echo "\n";
+
+// Division 
+$division = 12; // = assignment operator 
+// $division = $division / 12; // Alternative process for Division /=
+$division /= 2;
+echo $division;
+
+echo "\n";
+
+// Modulas 
+$modulas = 13; // = assignment operator 
+// $modulas = $modulas % 3; // Alternative process for Modulas %=
+$modulas %= 2;
+echo $modulas;
