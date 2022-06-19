28e790a4379599f186e115b0b04db020abd10fd4
ittoufiq <ittoufiq@gmail.com>
2022-06-20 04:30:57 +0600

    Multiple case alternative process

--- switchcase.php
+++ switchcase.php
@@ -30,3 +30,48 @@ switch($color){
         default:
         echo "We don't about this {$color}";
 }
+
+echo "\n";
+
+// how to find out +ve even+odd or -ve even+od number 
+
+$m = -13;
+$result = $m % 2;
+
+switch($result){
+    case 0:
+        switch($m){
+            case $m > 0:
+                echo "{$m} is positive even number";
+                break;
+            case $m < 0;
+                echo "{$m} is negative even number";
+        }
+        break;
+        default:
+        switch($m){
+            case $m > 0:
+                echo "{$m} is positive odd number";
+                break;
+            case $m < 0;
+                echo "{$m} is negative odd number";
+        }
+}
+
+echo "\n";
+
+// Multiple case alternative process 
+
+switch(true){
+    case ($result == 0 && $m > 0):
+        echo "{$m} is positive even number";
+        break;
+    case ($result == 1 && $m > 0):
+        echo "{$m} is positive odd number";
+        break;
+    case ($result == 0 && $m < 0):
+        echo "{$m} is negative even number";
+        break;
+    case ($result == -1 && $m < 0):
+        echo "{$m} is negative odd number";
+}
